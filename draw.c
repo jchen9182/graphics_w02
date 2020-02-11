709368fd9a4f6d9f09c930cd632216646cdccd0e
jchen9182 <jchen9182@gmail.com>
2020-02-10 22:03:32 -0500

    DONE

--- draw.c
+++ draw.c
@@ -8,59 +8,80 @@
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
     int x = x0;
     int y = y0;
-    int delta_y = y1 - y0; // A
-    int delta_x = x0 - x1; // B
-    float slope;
-    if (!delta_x) slope = 0;
-    else slope = delta_y / delta_x;
+    // Make sure we go from left to right
+    if (x0 > x1 && x0 != 0) {
+        x = x1;
+        x1 = x0;
+        y = y1;
+        y1 = y0;
+    }
+    int delta_y = y1 - y;
+    int delta_x = x1 - x;
+    int A = delta_y;
+    int B = -1 * delta_x;
+    int d;
+    printf("delta_y: %d, delta_x: %d\n", delta_y, delta_x);
 
-    if (slope > 0) {
-        // Octant 2 & 6
-        // if (slope > 1) {
-        //     int d = delta_y + 2 * delta_x;
-        //
-        //     while (y <= y1) {
-        //         plot(s, c, x, y);
-        //         if (d < 0) {
-        //             x++;
-        //             d += 2 * delta_y;
-        //         }
-        //         y++;
-        //         d += 2 * delta_x;
-        //     }
-        }
+    if (delta_y * delta_x >= 0) {
         // Octant 1 & 5
-        else {
-            int d = 2 * delta_y + delta_x;
+        if (delta_y <= delta_x) {
+            printf("OCTANT 1\n");
+            d = 2 * A + B;
 
             while (x <= x1) {
                 plot(s, c, x, y);
                 if (d > 0) {
                     y++;
-                    d += 2 * delta_x;
+                    d += 2 * B;
                 }
                 x++;
-                d += 2 * delta_y;
+                d += 2 * A;
+            }
+        }
+        // Octant 2 & 6
+        else {
+            printf("OCTANT 2\n");
+            d = A + 2 * B;
+        
+            while (y <= y1) {
+                plot(s, c, x, y);
+                if (d < 0) {
+                    x++;
+                    d += 2 * A;
+                }
+                y++;
+                d += 2 * B;
             }
         }
     }
     // Octant 8 & 4
-    else if (slope > -1) {
-        // int d = delta_y - 2 * delta_x;
-        //
-        // while (x <= x1) {
-        //     plot(s, c, x, y);
-        //     if (d > 0) {
-        //         x++;
-        //         d -= 2 * delta_y;
-        //     }
-        //     y--;
-        //     d += 2 * delta_x;
-        // }
+    else if (abs(delta_y) <= abs(delta_x)) {
+        printf("OCTANT 8\n");
+        d = A - 2 * B;
+        
+        while (x <= x1) {
+            plot(s, c, x, y);
+            if (d < 0) {
+                y--;
+                d -= 2 * B;
+            }
+            x++;
+            d += 2 * A;
+        } 
     }
     // Octant 7 & 3
     else {
+        printf("OCTANT 7\n");
+        d = 2 * A - B;
 
+        while (y >= y1) {
+            plot(s, c, x, y);
+            if (d > 0) {
+                x++;
+                d += 2 * A;
+            }
+            y--;
+            d -= 2 * B;
+        }
     }
-
 }
